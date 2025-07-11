0d61595dc389709d6ff93b21fee85b5a4c4ca20e
knmcguire <kimberleymcguire@gmail.com>
2025-07-11 14:26:41 +0200

    fix payload error crazyflie_linkcpp

--- crazyflie/src/crazyflie_server.cpp
+++ crazyflie/src/crazyflie_server.cpp
@@ -128,9 +128,9 @@ private:
     int16_t vx;
     int16_t vy;
     int16_t vz;
-    int16_t rateRoll;
-    int16_t ratePitch;
-    int16_t rateYaw;
+    //int16_t rateRoll;  
+    //int16_t ratePitch;
+    //int16_t rateYaw;
   } __attribute__((packed));
 
 
@@ -465,9 +465,9 @@ public:
                 {"stateEstimateZ", "vx"},
                 {"stateEstimateZ", "vy"},
                 {"stateEstimateZ", "vz"},
-                {"stateEstimateZ", "rateRoll"},
-                {"stateEstimateZ", "ratePitch"},
-                {"stateEstimateZ", "rateYaw"}
+                //{"stateEstimateZ", "rateRoll"},
+                //{"stateEstimateZ", "ratePitch"},
+                //{"stateEstimateZ", "rateYaw"}
               }, cb));
             log_block_odom_->start(uint8_t(100.0f / (float)freq)); // this is in tens of milliseconds
             
@@ -884,9 +884,9 @@ private:
       msg.twist.twist.linear.x = data->vx / 1000.0f;
       msg.twist.twist.linear.y = data->vy / 1000.0f;
       msg.twist.twist.linear.z = data->vz / 1000.0f;
-      msg.twist.twist.angular.x = data->rateRoll / 1000.0f;
-      msg.twist.twist.angular.y = data->ratePitch / 1000.0f;
-      msg.twist.twist.angular.z = data->rateYaw / 1000.0f;
+      //msg.twist.twist.angular.x = data->rateRoll / 1000.0f;
+      //msg.twist.twist.angular.y = data->ratePitch / 1000.0f;
+      //msg.twist.twist.angular.z = data->rateYaw / 1000.0f;
 
       publisher_odom_->publish(msg);
     }
