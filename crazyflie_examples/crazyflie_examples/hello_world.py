cfa76c5d41f5ac3b1d3984bda0889ee7f0894411
wyseo10 <realmeteny10@gmail.com>
2025-08-22 02:16:00 +0900

    test

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -10,7 +10,7 @@ HOVER_DURATION = 5.0
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
-    cf = swarm.allcfs.crazyflies[0]
+    cf = swarm.allcfs
 
     cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
